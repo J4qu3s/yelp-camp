4e5d477c320444a27b561206030c6cf2fbd15e1b
j4qu3s <jarkko.hoyhtya@gmail.com>
2021-09-10 14:22:55 +0300

    inTheMiddleOf506
    adding passport to project

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const localStrategy = require('passport-local')
 //Schemas
 const { campgroundSchema, reviewSchema } = require('./schemas');
 //models
 const Campground = require('./models/campground');
 const Review = require('./models/review');
+const User = require('./models/user');
 //routes
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -52,9 +55,20 @@ const sessionConfig = {
 
 }
 app.use(session(sessionConfig));
-
-//flash and it's middleware
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+//Use localstrategy and User model as authenticate method
+//Which is made by passport!
+passport.use(new localStrategy(User.authenticate()));
+
+//How to get user to session and get logged out of session
+//by passport local mongoose
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Flash middleware
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
